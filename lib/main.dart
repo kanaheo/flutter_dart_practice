49c48e40a3be3ac04fd6d836ebe050b8c9cf5aac
heo_kyoungmin <heo_kyoungmin@fk.cocone.co.jp>
2023-01-23 17:47:04 +0900

    widget life cycle

--- lib/main.dart
+++ lib/main.dart
@@ -35,10 +35,30 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    // build보다 먼저 실행되야 하며 주로 부모의 변수를 가지고 놀 때 사용
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    // 이건 뭔가 api 업데이트를 구독하게 해준다 ! 즉 감시?? 한다. 위젯 트리가 제거 될 때
+    // 화면에서 사라질 때 실행한다.
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
