c85c0cc4fd09e39c4c33117d1ebf047f9d243578
heo_kyoungmin <heo_kyoungmin@fk.cocone.co.jp>
2023-01-26 15:37:49 +0900

    use future builder for api data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +23,16 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
         centerTitle: true,
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          // snapshot는 future의 상태(로딩인지에러인지등등)
+          if (snapshot.hasData) {
+            return const Text("There is data!");
+          }
+          return const Text("Loading...");
+        },
       ),
     );
   }
