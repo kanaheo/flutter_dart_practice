ffa48e167b97edef91782ae81c4e5780667a6944
heo_kyoungmin <heo_kyoungmin@fk.cocone.co.jp>
2023-01-30 14:19:10 +0900

    webtoon hoom screen end

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,26 +30,72 @@ class HomeScreen extends StatelessWidget {
         builder: (context, snapshot) {
           // snapshot는 future의 상태(로딩인지에러인지등등)
           if (snapshot.hasData) {
-            return ScrollConfiguration(
-              behavior: ScrollConfiguration.of(context).copyWith(
-                dragDevices: {PointerDeviceKind.mouse},
-              ),
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemCount: snapshot.data!.length,
-                // 밑에 itemBuilder을 이용해서 내가 원할때마다 항목생성(한번에 생성 ㄴ)
-                itemBuilder: ((context, index) {
-                  var webtoon = snapshot.data![index];
-                  return Text(webtoon.title);
-                }),
-                separatorBuilder: (context, index) => const SizedBox(width: 20),
-              ),
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(
+                  child: makeList(context, snapshot),
+                )
+              ],
             );
           }
           return const Center(
             child: CircularProgressIndicator(),
           );
         },
+      ),
+    );
+  }
+
+  ScrollConfiguration makeList(
+      BuildContext context, AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ScrollConfiguration(
+      behavior: ScrollConfiguration.of(context).copyWith(
+        dragDevices: {PointerDeviceKind.mouse},
+      ),
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemCount: snapshot.data!.length,
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 20,
+        ),
+        // 밑에 itemBuilder을 이용해서 내가 원할때마다 항목생성(한번에 생성 ㄴ)
+        itemBuilder: ((context, index) {
+          var webtoon = snapshot.data![index];
+          return Column(
+            children: [
+              Container(
+                width: 250,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 3,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.5),
+                    ),
+                  ],
+                ),
+                child: Image.network(webtoon.thumb),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                webtoon.title,
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          );
+        }),
+        separatorBuilder: (context, index) => const SizedBox(width: 40),
       ),
     );
   }
